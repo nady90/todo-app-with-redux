0d2d82ebe9b11960684393415c1f0c8ca68b5304
Abanoub <naguib.abanoub@gmail.com>
2023-09-06 05:24:49 +0300

    changes the edit input to make it more user friendly

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -87,7 +87,7 @@ const TodoItem = ({ item }) => {
             onKeyDown={handleEditFieldKeyDown}
             type="text"
             className="edit-input"
-            placeholder={item.title}
+            defaultValue={item.title}
           />
           <div className="edit-icon-container" onClick={handleEdit}>
             <svg viewBox="0 0 62 62" xmlns="http://www.w3.org/2000/svg">
